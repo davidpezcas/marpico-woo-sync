c4c7507633adceeefc546a874d4ff0ba21af8b08
David Perez <davidpecast@gmail.com>
2025-09-04 14:30:22 -0500

    nueva version

--- marpico-woo-sync.php
+++ marpico-woo-sync.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Marpico Woo Sync
  * Description: Sincroniza productos y categorías desde la API de Marpico hacia WooCommerce.
- * Version: 1.0.3
+ * Version: 1.0.1
  * Author: David Perez
  * Author URI:  https://impactosdigitales.co
  * Plugin URI:  https://github.com/davidpezcas/marpico-woo-sync
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Definir constante de versión
 if ( ! defined( 'MARPICO_SYNC_VERSION' ) ) {
-    define( 'MARPICO_SYNC_VERSION', '1.0.3' );
+    define( 'MARPICO_SYNC_VERSION', '1.0.1' );
 }
 
 define( 'MARPICO_WOO_SYNC_PATH', plugin_dir_path( __FILE__ ) );
